218dd5661700dc1a924c1067eeb86ab6cd0be25e
Lonick32 <39029077+Lonick32@users.noreply.github.com>
2018-11-17 12:45:40 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,15 @@ from discord.ext import commands
 import asyncio
 import os
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!!')
 
 @bot.event
 async def on_ready():
-  print("Hey nigger smd!")
-
+  print("Hey look, I'm on!")
+  
 @bot.command()
-async def say (msg)
+async def say(msg):
   await bot.say(msg)
-
-
-
+  
+  
 bot.run(os.environ['TOKEN'])
